da722a32e06c3704b5b8b4bd47f993daebba7801
alfonsovleon <55829955+alfonsovleon@users.noreply.github.com>
2019-11-29 11:05:24 +0100

    Add files via upload

---
+++ WebApplication1.Services/Stockcaje/DelegacionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApplication1.Model;
+
+namespace WebApplication1.Services.Stockcaje
+{
+    public class DelegacionService : IDelegacionService
+    {
+        private readonly IRepository<Delegacion> delegacionRepository;
+        public DelegacionService(IRepository<Delegacion> delegacionRepository)
+        {
+            this.delegacionRepository = delegacionRepository;
+        }
+        public Delegacion AddDelegacion(Delegacion delegacion)
+        {
+            return delegacionRepository.Add(delegacion);
+        }
+        public List<Delegacion> Search(string query, int pageNumber, int pageSize)
+        {
+            return delegacionRepository.Table
+                .Where(delegacion => delegacion.Name.Contains(query) || delegacion.Code.Contains(query))
+                .OrderByDescending(delegacion => delegacion.UpdateDate)
+                .Skip(pageNumber * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+    }
+    
+}
---
+++ WebApplication1.Services/Stockcaje/IDelegacionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Model;
+
+namespace WebApplication1.Services.Stockcaje
+{
+    public interface IDelegacionService
+    {
+        Delegacion AddDelegacion(Delegacion delegacion);
+        List<Delegacion> Search(string query, int pageNumber, int pageSize);
+    }
+}
